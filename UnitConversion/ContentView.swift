bb77c70b6af702b6ad1a3e1cfc725e51a2c40f1f
Kevin Gutowski <kskiviolin@gmail.com>
2020-07-27 19:42:27 -0700

    Initial Commit

---
+++ UnitConversion/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  UnitConversion
+//
+//  Created by Kevin Gutowski on 7/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Kevin Gutowski on 7/27/20.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
